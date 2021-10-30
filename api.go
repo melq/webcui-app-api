7505986fc175cff98f5efb849778277371fbc804
melq <tu2sice@yahoo.co.jp>
2021-10-30 22:10:35 +0900

    :star: ipconfigの結果を返すようにした

--- api.go
+++ api.go
@@ -3,17 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os/exec"
+	"strings"
 )
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
 
 	log.Println("handleRoot called")
-	if _, err := fmt.Fprintf(w, "this is route_api"); err != nil {
+
+	res, err := exec.Command("ipconfig").Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	iostr := strings.NewReader(string(res))
+	rio := transform.NewReader(iostr, japanese.ShiftJIS.NewDecoder())
+	str, err := ioutil.ReadAll(rio)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if _, err := fmt.Fprintf(w, string(str)); err != nil {
 		log.Fatalln(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module route_api
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
